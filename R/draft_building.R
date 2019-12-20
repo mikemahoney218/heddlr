1f732597a37c58351420603c8c5d023efe10b097
Mike Mahoney <mike.mahoney.218@gmail.com>
2019-12-20 18:48:08 -0500

    Add create_yaml_header function

--- R/draft_building.R
+++ R/draft_building.R
@@ -40,3 +40,67 @@ assemble_draft <- function(patterns) {
   }
   draft
 }
+
+#' Convert list objects into R Markdown YAML headers
+#'
+#' This function tweaks the behavior of yaml::as.yaml to return a string which
+#' can immediately be used as an R Markdown YAML header. It's designed to
+#' accept both deeply nested lists and simpler list formats to make
+#' reasoning about your header easier.
+#'
+#' @param ... A set of objects that will be combined into the YAML header.
+#' Objects may be provided as lists (the structure
+#' list("outputs" = "html_document") translates to outputs: html_document)
+#' or as single-item named vectors (passing "title" = "My Report" to ... will
+#' translate to title: "My Report").
+#'
+#' @param line.sep,indent,omap,column.major,unicode,precision,indent.mapping.sequence,handlers Additional
+#' arguments to be passed to as.yaml (and documented within the yaml package)
+#'
+#' @return A string, formatted for use as an R Markdown YAML header
+#'
+#' @export
+
+create_yaml_header <- function(...,
+                               line.sep = c("\n", "\r\n", "\r"),
+                               indent = 2,
+                               omap = FALSE,
+                               column.major = TRUE,
+                               unicode = TRUE,
+                               precision = getOption("digits"),
+                               indent.mapping.sequence = FALSE,
+                               handlers = NULL) {
+  args <- list(...)
+  if (length(args) == 0) {
+    stop("Argument 1 must not be empty.")
+  }
+
+  header.content <- vector("list")
+  for (i in seq_along(args)) {
+    if (!is.list(args[[i]])) {
+      stop("All arguments must be a list.")
+    }
+
+    if (names(args[i]) == "") {
+      header.content <- c(header.content, args[[i]])
+    } else {
+      header.content <- c(header.content, args[i])
+    }
+  }
+
+  paste0(
+    "---\n",
+    yaml::as.yaml(
+      header.content,
+      line.sep,
+      indent,
+      omap,
+      column.major,
+      unicode,
+      precision,
+      indent.mapping.sequence,
+      handlers
+    ),
+    "---\n"
+  )
+}
