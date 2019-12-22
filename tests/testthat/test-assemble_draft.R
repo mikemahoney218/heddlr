505b3f3857311e82d6f098bfea9a053240398394
Mike Mahoney <mike.mahoney.218@gmail.com>
2019-12-22 18:23:31 -0500

    Replace placeholder variable with real object

--- tests/testthat/test-assemble_draft.R
+++ tests/testthat/test-assemble_draft.R
@@ -9,7 +9,7 @@ test_that("assemble_draft fails when it should", {
 
 test_that("assemble_draft imports objects as expected", {
   draft <- assemble_draft("one" = "../rmd/sample_pattern.Rmd", "two" = "../rmd/sample_pattern.Rmd")
-  expect_equal(length(x),
+  expect_equal(length(draft),
                2)
   expect_match(names(draft)[[1]], "one")
   expect_match(names(draft)[[2]], "two")
