c642b8f03f6e0ef424db60a6a967298b8311f377
Michael Mahoney <mike.mahoney.218@gmail.com>
2020-01-29 18:11:31 -0500

    Add logic to remove carriage returns (#24)
    This PR fixes issue #23

--- tests/testthat/test-export_template.R
+++ tests/testthat/test-export_template.R
@@ -10,10 +10,64 @@ test_that("export_template exports a template", {
   expect_equal(
     nchar(paste0(capture.output(export_template(pattern,
       stdout(),
-      filename.is.string = F
+      filename.is.string = FALSE
     )),
     collapse = "\n"
     )) + 1,
     nchar(pattern)
+  )
+})
+
+test_that("strip.carriage.returns only strips carriage returns", {
+  pattern <- import_pattern("../rmd/sample_pattern.Rmd")
+  expect_equal(
+    nchar(
+      paste0(
+        capture.output(
+          export_template(
+            pattern,
+            stdout(),
+            filename.is.string = FALSE
+          )
+        ),
+        collapse = "\n"
+      )
+    ),
+    nchar(
+      paste0(
+        capture.output(
+          export_template(
+            pattern,
+            stdout(),
+            filename.is.string = FALSE,
+            strip.carriage.returns = FALSE
+          )
+        ),
+        collapse = "\n"
+      )
+    )
+  )
+
+  expect_match(
+    paste0(
+      capture.output(
+        export_template(
+          "text\ntext",
+          stdout(),
+          filename.is.string = FALSE
+        )
+      ),
+      collapse = "\n"
+    ),
+    paste0(
+      capture.output(
+        export_template(
+          "text\r\ntext",
+          stdout(),
+          filename.is.string = FALSE
+        )
+      ),
+      collapse = "\n"
+    )
   )
 })
